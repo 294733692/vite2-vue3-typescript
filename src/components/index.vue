0ffa90f856d2754c68f53cfa6b3c308e975cc1da
yyc <18113406084@163.com>
2021-04-22 13:36:43 +0800

    vue3 路由配置

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="main-container">
+    <div class="top">
+      顶部
+    </div>
+    <div class="bottom">底部</div>
+    <div class="right">
+      <div class="content">
+          内容区域
+        <router-view/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped>
+
+</style>
