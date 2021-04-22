0195c19bddfca478b10864abc680ed34a7d4adb1
yyc <18113406084@163.com>
2021-04-22 11:54:45 +0800

    项目基础配置，项目目录结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 设置 "@" 指向 "src" 目录
+    }
+  },
+  base: './', // 设置打包目录
+  server: {
+    port: 3000, // 设置端口号
+    open: true, // 启动服务，是否自动打开浏览器
+    cors: true, // 是否允许跨域
+
+    // 请求代理， 根据项目实际情况配置
+    proxy: {
+      '/api': {
+        target: 'http://xxx.com:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace('/api/', '/'), // 接口重写
+      }
+    }
+  }
 })
